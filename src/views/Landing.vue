0eeced394dc20c0878d1559ac9dd571687e001d6
shuhang-cai <shuhang96@hotmail.com>
2020-10-19 17:06:16 +0800

    Cleaned up Landing page, and added new images to test. Did some work on Navbar as well.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -6,20 +6,19 @@
           <div
             class="md-layout-item md-size-50 md-small-size-70 md-xsmall-size-100"
           >
-            <h1 class="title">Your Story Starts With Us.</h1>
-            <h4>
-              Every landing page needs a small description after the big bold
-              title, that's why we added this text here. Add here all the
-              information that can make you or your product create the first
-              impression.
-            </h4>
+            <h1 class="title">Upgrade your career </h1>
+            <h3>
+              Discover online courses to develop your professional skillsets.
+            </h3>
             <br />
             <md-button
-              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              href="/quiz/lifestage"
               class="md-success md-lg"
               target="_blank"
-              ><i class="fas fa-play"></i> Watch video</md-button
             >
+              <i class="fas fa-play"></i> 
+              Get Started
+            </md-button>
           </div>
         </div>
       </div>
@@ -31,54 +30,45 @@
             <div
               class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-center"
             >
-              <h2 class="title text-center">Let's talk product</h2>
-              <h5 class="description">
-                This is the paragraph where you can write more details about
-                your product. Keep you user engaged by providing meaningful
-                information. Remember that by this time, the user is curious,
-                otherwise he wouldn't scroll to get here. Add a button if you
-                want the user to see more.
-              </h5>
+              <h2 class="title text-center">Times are tough</h2>
+              <h4 class="description">
+                There are many resources and courses available for this on various platforms like SkillsFuture and Coursera. Despite the plethora of choices
+                 online and offline, people may be unsure of which courses are applicable or useful to their industry. 
+              </h4>
             </div>
           </div>
           <div class="features text-center">
             <div class="md-layout">
               <div class="md-layout-item md-medium-size-33 md-small-size-100">
                 <div class="info">
-                  <div class="icon icon-info">
-                    <md-icon>chat</md-icon>
+                  <div class="icon icon-danger">
+                    <md-icon>fact_check</md-icon>
                   </div>
-                  <h4 class="info-title">Free Chat</h4>
+                  <h4 class="info-title">Discover yourself</h4>
                   <p>
-                    Divide details about your product or agency work into parts.
-                    Write a few lines about each one. A paragraph describing a
-                    feature will be enough.
+                    Complete our onboarding quiz to let us know what industries you are interested in.
                   </p>
                 </div>
               </div>
               <div class="md-layout-item md-medium-size-33 md-small-size-100">
                 <div class="info">
                   <div class="icon icon-success">
-                    <md-icon>verified_user</md-icon>
+                    <md-icon>rule</md-icon>
                   </div>
-                  <h4 class="info-title">Verified Users</h4>
+                  <h4 class="info-title">Fill in the gaps</h4>
                   <p>
-                    Divide details about your product or agency work into parts.
-                    Write a few lines about each one. A paragraph describing a
-                    feature will be enough.
+                    Our proprietary algorithm will recommend a personalized collection of online learning resources that is tailored to your career path.
                   </p>
                 </div>
               </div>
               <div class="md-layout-item md-medium-size-33 md-small-size-100">
                 <div class="info">
-                  <div class="icon icon-danger">
-                    <md-icon>fingerprint</md-icon>
+                  <div class="icon icon-info">
+                    <md-icon>menu_book</md-icon>
                   </div>
-                  <h4 class="info-title">Fingerprint</h4>
+                  <h4 class="info-title">Lifelong learning</h4>
                   <p>
-                    Divide details about your product or agency work into parts.
-                    Write a few lines about each one. A paragraph describing a
-                    feature will be enough.
+                    Upgrade your skills to land the job you deserve.
                   </p>
                 </div>
               </div>
@@ -91,7 +81,41 @@
           <h2 class="title">Here is our team</h2>
           <div class="team">
             <div class="md-layout">
-              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+              <div class="md-layout-item md-medium-size-50 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg1"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Cai Shuhang
+                      <br />
+                      <small class="card-description text-muted">Scrum Master</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        Def not sus 
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="https://www.linkedin.com/in/cai-shuhang/"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-linkedin"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+              
+              <div class="md-layout-item md-medium-size-50 md-small-size-100">
                 <div class="team-player">
                   <md-card class="md-card-plain">
                     <div class="md-layout-item md-size-50 mx-auto">
@@ -139,7 +163,7 @@
                   </md-card>
                 </div>
               </div>
-              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+              <div class="md-layout-item md-medium-size-50 md-small-size-100">
                 <div class="team-player">
                   <md-card class="md-card-plain">
                     <div class="md-layout-item md-size-50 mx-auto">
@@ -183,7 +207,7 @@
                   </md-card>
                 </div>
               </div>
-              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+              <div class="md-layout-item md-medium-size-50 md-small-size-100">
                 <div class="team-player">
                   <md-card class="md-card-plain">
                     <div class="md-layout-item md-size-50 mx-auto">
@@ -235,46 +259,6 @@
           </div>
         </div>
       </div>
-      <div class="section section-contacts">
-        <div class="container">
-          <div class="md-layout">
-            <div class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto">
-              <h2 class="text-center title">Work with us</h2>
-              <h4 class="text-center description">
-                Divide details about your product or agency work into parts.
-                Write a few lines about each one and contact us about any
-                further collaboration. We will responde get back to you in a
-                couple of hours.
-              </h4>
-              <form class="contact-form">
-                <div class="md-layout">
-                  <div class="md-layout-item md-size-50">
-                    <md-field>
-                      <label>Your Name</label>
-                      <md-input v-model="name" type="text"></md-input>
-                    </md-field>
-                  </div>
-                  <div class="md-layout-item md-size-50">
-                    <md-field>
-                      <label>Your Email</label>
-                      <md-input v-model="email" type="email"></md-input>
-                    </md-field>
-                  </div>
-                </div>
-                <md-field maxlength="5">
-                  <label>Your Message</label>
-                  <md-textarea v-model="message"></md-textarea>
-                </md-field>
-                <div class="md-layout">
-                  <div class="md-layout-item md-size-33 mx-auto text-center">
-                    <md-button class="md-success">Send Message</md-button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -285,17 +269,21 @@ export default {
   props: {
     header: {
       type: String,
-      default: require("@/assets/img/bg7.jpg")
+      default: require("@/assets/img/hero2.jpg")
     },
     teamImg1: {
       type: String,
-      default: require("@/assets/img/faces/avatar.jpg")
+      default: require("@/assets/img/faces/among_us.png")
     },
     teamImg2: {
       type: String,
       default: require("@/assets/img/faces/christian.jpg")
     },
     teamImg3: {
+      type: String,
+      default: require("@/assets/img/faces/kendall.jpg")
+    },
+    teamImg4: {
       type: String,
       default: require("@/assets/img/faces/kendall.jpg")
     }
