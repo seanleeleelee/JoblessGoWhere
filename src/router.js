63b7d37f9e975ef5170fbf681108b6367d0368da
shuhang-cai <shuhang96@hotmail.com>
2020-10-20 11:53:54 +0800

    Added new line to QuizLifestage and removed footer

--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ import Landing from "./views/Landing.vue";
 import Login from "./views/Login.vue";
 import Profile from "./views/Profile.vue";
 import MainNavbar from "./layout/MainNavbar.vue";
-import MainFooter from "./layout/MainFooter.vue";
 import QuizLifestage from "./views/QuizLifestage.vue";
 
 Vue.use(Router);
@@ -15,7 +14,7 @@ export default new Router({
     {
       path: "/",
       name: "index",
-      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      components: { default: Index, header: MainNavbar },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
@@ -24,7 +23,7 @@ export default new Router({
     {
       path: "/landing",
       name: "landing",
-      components: { default: Landing, header: MainNavbar, footer: MainFooter },
+      components: { default: Landing, header: MainNavbar },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
@@ -33,7 +32,7 @@ export default new Router({
     {
       path: "/login",
       name: "login",
-      components: { default: Login, header: MainNavbar, footer: MainFooter },
+      components: { default: Login, header: MainNavbar },
       props: {
         header: { colorOnScroll: 400 }
       }
@@ -41,7 +40,7 @@ export default new Router({
     {
       path: "/profile",
       name: "profile",
-      components: { default: Profile, header: MainNavbar, footer: MainFooter },
+      components: { default: Profile, header: MainNavbar },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
@@ -50,7 +49,7 @@ export default new Router({
     {
       path: "/quiz/lifestage",
       name: "quizlifestage",
-      components: { default: QuizLifestage, header: MainNavbar, footer: MainFooter },
+      components: { default: QuizLifestage, header: MainNavbar },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
