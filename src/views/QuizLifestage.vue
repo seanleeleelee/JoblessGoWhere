a73137244b19eac67c89c080629fa9b37f52a8f6
shuhang.cai <shuhang96@hotmail.com>
2020-10-24 17:10:51 +0800

    Updated photos and code for quiz page 1

--- src/views/QuizLifestage.vue
+++ src/views/QuizLifestage.vue
@@ -6,115 +6,120 @@
             <div class=  "section">
                 <div class= "container">
                     <div class="md-layout">
-                        <div 
-                        class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-center"
-                        >
+                        <div class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-center">
                             <h2 class="title text-center">What stage in life are you at?</h2>
-                            <div class="lifestages">
                                 <div class="md-layout">
                                     <div class="md-layout-item md-medium-size-25 md-small-size-100">
-                                        <div class="lifestage">
-                                            <md-card class="md-card-plain">
-                                                <div class="md-layout-item md-size-100 mx-auto">
-                                                <img
-                                                    :src="studentImg"
-                                                    alt="Thumbnail Image"
-                                                    class="img-raised rounded"
-                                                />
-                                                </div>
-                                                <h4 class="card-title">
-                                                Student
-                                                </h4>
+                                        <div class="lifestage" @click="onClickStudent()" :class="{border:lifestage == 'Student'}">
+                                            <md-card class="md-card-plain" md-with-hover>
+                                                <md-ripple>
+                                                    <md-card-media>
+                                                        <img
+                                                            :src="studentImg"
+                                                            alt="Image of student with books"
+                                                            class="img-raised rounded"
+                                                        />
+                                                    </md-card-media>
+                                                    <md-card-content>
+                                                        <h4 class="card-title">
+                                                            Student
+                                                        </h4>
+                                                        <p class="card-description">
+                                                            Still in school but you want to be one step ahead of your peers
+                                                        </p>
+                                                    </md-card-content>
+                                                </md-ripple>
+                                            </md-card>
+                                        </div>
+                                    </div>  
+
+                                    <div class="md-layout-item md-medium-size-25 md-small-size-100">
+                                        <div class="lifestage" @click="onClickGrad()" :class="{border:lifestage == 'Graduate'}">
+                                        <md-card class="md-card-plain" md-with-hover>
+                                            <md-ripple>
+                                                <md-card-media>
+                                                    <img
+                                                        :src="graduateImg2"
+                                                        alt="Image of a fresh graduate"
+                                                        class="img-raised rounded "
+                                                    />
+                                                </md-card-media>
                                                 <md-card-content>
-                                                <p class="card-description">
-                                                    Still in school but you want to be one step ahead of your peers
-                                                </p>
+                                                    <h4 class="card-title">
+                                                    Fresh Graduate
+                                                    </h4>
+                                                    <p class="card-description">
+                                                        Just graduated and improving skillsets to find a job
+                                                    </p>
                                                 </md-card-content>
+                                            </md-ripple>
+                                        </md-card>
+                                        </div>
+                                    </div>
+                                    <div class="md-layout-item md-medium-size-25 md-small-size-100">
+                                        <div class="lifestage" @click="onClickExp1()" :class="{border:lifestage == 'Early Professional'}">
+                                            <md-card class="md-card-plain" md-with-hover>
+                                                <md-ripple>
+                                                    <md-card-media>
+                                                        <img
+                                                            :src="industryImg3"
+                                                            alt="Thumbnail Image"
+                                                            class="img-raised rounded img-fluid"
+                                                        />
+                                                    </md-card-media>
+                                                    <md-card-content>
+                                                        <h4 class="card-title">
+                                                        Early Career
+                                                        </h4>
+                                                        <p class="card-description">
+                                                            Have some work experience but you are looking to switch industries
+                                                        </p>
+                                                    </md-card-content>
+                                                </md-ripple>
                                             </md-card>
                                         </div>
                                     </div>
 
                                     <div class="md-layout-item md-medium-size-25 md-small-size-100">
-                                        <div class="lifestage">
-                                        <md-card class="md-card-plain">
-                                            <div class="md-layout-item md-size-100 mx-auto">
-                                            <img
-                                                :src="graduateImg2"
-                                                alt="Thumbnail Image"
-                                                class="img-raised rounded "
-                                            />
-                                            </div>
-                                            <h4 class="card-title">
-                                            Fresh Graduate
-                                            </h4>
-                                            <md-card-content>
-                                            <p class="card-description">
-                                                Just graduated and improving skillsets to find a job
-                                            </p>
-                                            </md-card-content>
-                                        </md-card>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="md-layout-item md-medium-size-25 md-small-size-100">
-                                        <div class="lifestage">
-                                        <md-card class="md-card-plain">
-                                            <div class="md-layout-item md-size-100 mx-auto">
-                                            <img
-                                                :src="industryImg3"
-                                                alt="Thumbnail Image"
-                                                class="img-raised rounded img-fluid"
-                                            />
-                                            </div>
-                                            <h4 class="card-title">
-                                            Keen in new industry
-                                            </h4>
-                                            <md-card-content>
-                                            <p class="card-description">
-                                                Have some work experience but you are looking to switch industries
-                                            </p>
-                                            </md-card-content>
-                                        </md-card>
-                                        </div>
-                                    </div>
-                                    <div class="md-layout-item md-medium-size-25 md-small-size-100">
-                                        <div class="lifestage">
-                                        <md-card class="md-card-plain">
-                                            <div class="md-layout-item md-size-100 mx-auto">
-                                            <img
-                                                :src="midCareerImg4"
-                                                alt="Thumbnail Image"
-                                                class="img-raised rounded img-fluid"
-                                            />
-                                            </div>
-                                            <h4 class="card-title">
-                                            Mid Career
-                                            </h4>
-                                            <md-card-content>
-                                            <p class="card-description">
-                                                An industry veteran that is keeping abreast of the latest developments
-                                            </p>
-                                            </md-card-content>
-                                        </md-card>
+                                        <div class="lifestage" @click="onClickExp2()" :class="{border:lifestage == 'Experienced Professional'}">
+                                            <md-card class="md-card-plain" md-with-hover>
+                                                <md-ripple>
+                                                    <md-card-media>
+                                                        <img
+                                                            :src="midCareerImg4"
+                                                            alt="Thumbnail Image"
+                                                            class="img-raised rounded img-fluid"
+                                                        />
+                                                    </md-card-media>
+                                                    <md-card-content>
+                                                        <h4 class="card-title">
+                                                        Mid Career
+                                                        </h4>
+                                                        <p class="card-description">
+                                                            An industry veteran that is keeping abreast of the latest developments
+                                                        </p>
+                                                    </md-card-content>
+                                                </md-ripple>
+                                            </md-card>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                        </div>
+
+                        <div class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-right">
                             <md-button
                                 href="/quiz/industry"
                                 class="md-success md-lg"
                             >
                                 <i class="fas fa-play"></i> 
-                                Next
-                            </md-button>
-
-                            
-                        </div>
+                                      Next
+                            </md-button>      
+                        </div>                     
                     </div>  
                 </div>
             </div>
         </div>
-        
+
     </div>
 </template>
 
@@ -148,11 +153,25 @@ export default {
             return {
                 backgroundImage: `url(${this.header})`
             };
+        },        
+    },
+    methods : {
+        onClickStudent(){
+            this.lifestage = "Student";
+        },
+        onClickGrad(){
+            this.lifestage = "Graduate";
+        },
+        onClickExp1(){
+            this.lifestage = "Early Professional";
+        },
+        onClickExp2(){
+            this.lifestage = "Experienced Professional";
         }
     },
     data() {
         return { 
-            lifestage: null, 
+            lifestage: null
         }
     }
 };
@@ -162,10 +181,15 @@ export default {
 
 .md-card-actions.text-center {
   display: flex;
-  justify-content: left !important;
+//   justify-content: left !important;
 }
 
 .md-has-textarea + .md-layout {
   margin-top: 20px;
+}
+
+.border {
+  border: 2px solid black;
+  border-radius: 15px
 }
 </style>
